cc9ccecc361551866c742befd7174f4c41d89425
Abiel Fernandez <abielfdz@fb.com>
2022-06-29 17:56:05 -0700

    Added MUI

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from "@mui/material/CssBaseline";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <React.Fragment>
+            <CssBaseline/>
+            <App/>
+        </React.Fragment>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
