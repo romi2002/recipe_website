9816ceb675f7b6f5c54f5d3d1086648b3b11cea4
Abiel Fernandez <abielfdz@fb.com>
2022-07-05 17:49:29 -0700

    Added recipe card to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import * as React from "react"
 import './App.css';
 import Button from '@mui/material/Button';
+import Navbar from './components/Navigation/Navbar'
+import RecipeCard from "./components/RecipeCard";
 
 
 function App() {
     return (
         <div className="App">
+            <Navbar/>
+            <RecipeCard/>
             <Button variant="contained">Hello World</Button>
         </div>
     );
