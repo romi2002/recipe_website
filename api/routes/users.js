2920f628b27a94a23ae8a91eb61983a6d347eb50
Abiel Fernandez <abielfdz@fb.com>
2022-07-07 16:53:53 -0700

    Refactored into separate Authentication class and switched to using JWT

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,17 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {body, validationResult} = require('express-validator')
-const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb")
-
-const uri = "mongodb://admin:admin@localhost:27017"
-const client = new MongoClient(uri, {serverApi: ServerApiVersion.v1})
-
-const crypto = require('crypto')
-
-client.connect()
-const database = client.db("recipe_app")
-const users = database.collection("users")
-
+const authentication = require('../models/Authentication')
 
 /* GET users listing. */
 router.post('/createUser',
@@ -23,16 +13,7 @@ router.post('/createUser',
             return res.status(400).send({errors: errors.array()})
         }
 
-        //Generate salt and hash password
-        const salt = crypto.randomBytes(16).toString('hex')
-        const hash = crypto.pbkdf2Sync(req.body.password, salt, 1000, 64, 'sha512').toString('hex')
-
-        //TODO check if a user does not already exist with the same email
-        await users.insertOne({
-            'username': req.body.email,
-            'hash': hash,
-            'salt': salt
-        })
+        await authentication.createUser(req.body.email, req.body.password)
 
         res.status(200).send({data: "user-created"})
     })
@@ -42,42 +23,23 @@ router.post('/login',
     body("password").isLength({min: 5}),
     async (req, res) => {
         const errors = validationResult(req)
-        if(!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             return res.status(400).send({errors: errors.array()})
         }
 
-        const user = await users.findOne({'username' : req.body.email})
-
-        if(user == null) {
-            return res.status(400).send({errors: "Invalid username/password"})
-        }
-
-        const hash = crypto.pbkdf2Sync(req.body.password, user.salt, 1000, 64, 'sha512').toString('hex')
-        if(hash !== user.hash){
-            return res.status(400).send({errors: "Invalid username/password"})
-        }
-
-        req.session.loggedIn = true
-        req.session.username = req.body.username
-
-        res.status(200).send({data: "user-login"})
+        res.status(200).send(await authentication.loginUser(req.body.email, req.body.password))
     })
 
 /*
-Simple route in which checks if the user is logged in
+Simple route in which checks if the token is valid
  */
-router.get('/isLoggedIn', (req, res) => {
-    res.status(200).send({loggedIn: req.session.loggedIn ?? false})
-})
+router.get('/is_valid_token', body("token").exists(), (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).send({errors: errors.array()})
+    }
 
-/*
-When called, route logs out the user if it is currently logged in
- */
-router.post('/logout', (req, res) => {
-    //TODO Is it necessary to throw an error when logout is called and the user is not logged in?
-    req.session.loggedIn = false
-
-    res.status(200).send() //TODO Send something back?
+    res.status(200).send({validToken: authentication.verifyToken(req.body.token)})
 })
 
 module.exports = router
