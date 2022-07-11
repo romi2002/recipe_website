5e08b01893389a5678c358dfd009ea4913a4859b
Abiel Fernandez <empoleom@gmail.com>
2022-07-11 14:49:41 -0700

    Added basic user creation / authentication (#2)
    * Added basic recipe routes, still needs basic error handling and query capabilities
    
    * Added express-validator
    
    * Added createUser route
    
    * Added basic login functionality
    
    * Fixed typo and variable declaration
    
    * Added user session handling
    
    * Added logout route
    
    * Merged
    
    * Fixed if user is null
    
    * Added password hashing to authentication
    
    * Refactored into separate Authentication class and switched to using JWT
    
    Co-authored-by: Abiel Fernandez <abielfdz@fb.com>

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const session = require('express-session')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -19,6 +20,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+
+//app.set('trust proxy', 1)
+app.use(session({
+    secret: 'qwerty',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {secure: false}
+}))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
