0364560b313a765bf36db16af69f571c8c28bfad
Abiel Fernandez <abielfdz@fb.com>
2022-07-12 11:38:08 -0700

    Modified floating button styling

--- client/src/components/Recipe/RecipeEditor.jsx
+++ client/src/components/Recipe/RecipeEditor.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import InstructionEditor from "./Instructions/InstructionEditor"
 import {useState} from "react"
 import IngredientEditor from "./Ingredients/IngredientEditor"
+import {Box} from "@mui/material"
 
 const RecipeEditor = () => {
     const [instructions, setInstructions] = useState([''])
@@ -10,9 +11,12 @@ const RecipeEditor = () => {
     return (
         <div>
             <h1>Editor</h1>
-            <InstructionEditor instructions={instructions}
-                               setInstructions={setInstructions}/>
-            <IngredientEditor ingredients={ingredients} setIngredients={setIngredients}/>
+            <Box sx={{display: 'flex'}}>
+                <InstructionEditor instructions={instructions}
+                                   setInstructions={setInstructions}/>
+                <IngredientEditor ingredients={ingredients} setIngredients={setIngredients}/>
+            </Box>
+
         </div>
     )
 }
--- client/src/components/Utils/EditableTable.jsx
+++ client/src/components/Utils/EditableTable.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody} from "@mui/material"
+import {Box, Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody} from "@mui/material"
 import {Fab} from "@mui/material"
 import AddIcon from '@mui/icons-material/Add'
 import RemoveIcon from '@mui/icons-material/Remove'
@@ -11,26 +11,42 @@ const EditableTable = ({
                            onRemoveHandler
                        }) => {
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {columns.map((col, index) => {
-                            return (<TableCell key={'editable-table-' + col + index}>{col}</TableCell>)
-                        })}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {children}
-                </TableBody>
-            </Table>
-            <Fab onClick={onAddHandler} color="primary" aria-label="add">
-                <AddIcon/>
-            </Fab>
-            <Fab onClick={onRemoveHandler} color="primary" aria-label="remove">
-                <RemoveIcon/>
-            </Fab>
-        </TableContainer>
+        <Box>
+
+            <TableContainer sx={{zIndex: 0}} component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((col, index) => {
+                                return (<TableCell key={'editable-table-' + col + index}>{col}</TableCell>)
+                            })}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {children}
+
+                    </TableBody>
+
+                </Table>
+                <Box sx={{display: 'flex',
+                    flexDirection: 'row',
+                    zIndex: 1,
+                    bottom: 0,
+                    right: 1,
+                    justifyContent: 'end',
+                    position: 'relative'}}>
+                    <Fab onClick={onAddHandler} color="primary" aria-label="add">
+                        <AddIcon/>
+                    </Fab>
+                    <Fab onClick={onRemoveHandler} color="primary" aria-label="remove">
+                        <RemoveIcon/>
+                    </Fab>
+                </Box>
+
+            </TableContainer>
+
+        </Box>
+
     )
 }
 
