efa5c16563444bfafeb864686d32a19947708971
Abiel Fernandez <abielfdz@fb.com>
2022-07-21 13:40:57 -0700

    Elasticsearch works now :)

--- api/utils/mongoUtil.js
+++ api/utils/mongoUtil.js
@@ -1,12 +1,14 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
 
-const uri = 'mongodb://admin:admin@localhost:27017'
-const _db = new MongoClient(uri, { serverApi: ServerApiVersion.v1 })
+const uri = 'mongodb://admin:admin@localhost:27017/?directConnection=true'
+const _db = new MongoClient(uri, { serverApi: ServerApiVersion.v1, serverSelectionTimeoutMS: 500 })
 
 module.exports = {
   connectToServer: () => {
     // TODO Handle errors when connecting to server
-    _db.connect()
+    _db.connect(err => {
+      console.log(err)
+    })
   },
 
   getDb: () => {
