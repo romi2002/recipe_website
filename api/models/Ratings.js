77b685639fa53ccbdd49891031e1a67a71ebcd63
Abiel Fernandez <abielfdz@fb.com>
2022-07-25 15:58:19 -0700

    Switched rating avg method

--- api/models/Ratings.js
+++ api/models/Ratings.js
@@ -1,8 +1,11 @@
 const mongoUtil = require('../utils/mongoUtil')
+const { ObjectId } = require('mongodb')
 
 const client = mongoUtil.getDb()
 const database = client.db('recipe_app')
+const recipes = database.collection('recipes')
 const ratings = database.collection('ratings')
+const ratingsAvg = database.collection('ratings_average')
 
 class Ratings {
   static async addRating (userData, recipeId, rating) {
@@ -12,6 +15,35 @@ class Ratings {
       recipeId,
       rating
     })
+
+    // run aggregation on affected recipe
+    await recipes.aggregate([{
+      $match: {
+        _id: new ObjectId(recipeId)
+      }
+    }, {
+      $addFields: {
+        recipeId: {
+          $toString: '$_id'
+        }
+      }
+    }, {
+      $lookup: {
+        from: 'ratings', localField: 'recipeId', foreignField: 'recipeId', as: 'ratings'
+      }
+    }, {
+      $set: {
+        average_rating: {
+          $avg: '$ratings.rating'
+        }
+      }
+    }, {
+      $unset: ['ratings', 'recipeId']
+    }, {
+      $merge: {
+        into: 'recipes'
+      }
+    }]).toArray()
   }
 }
 
