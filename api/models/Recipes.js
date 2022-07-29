6078fa6921c2def6ab42db094fee8b674d46b6b3
Abiel Fernandez <abielfdz@fb.com>
2022-07-28 17:37:58 -0700

    Added keyword processing function

--- api/models/Recipes.js
+++ api/models/Recipes.js
@@ -4,7 +4,26 @@ const client = mongoUtil.getDb()
 const database = client.db('recipe_app')
 const recipes = database.collection('recipes')
 
+const ingredientDb = database.collection('ingredients').find({}).toArray()
+
 module.exports = {
+  /**
+   * Process ingredients and associate ids
+   * @param recipeIngredients
+   * @returns {Promise<void>}
+   */
+  processRecipeIngredients: async (recipeIngredients) => {
+    /**
+     * For each ingredient string, compare to all ingredients in db
+     * keyword will be the ingredient with the largest length
+     */
+    return Promise.all(recipeIngredients.map(async (ingredient) => (
+      (await ingredientDb)
+        .filter(i => ingredient.toLowerCase().includes(i.text))
+        .sort((a, b) => b.text.length - a.text.length)?.[0]
+    )))
+  },
+
   /**
    * Express middleware, gets recipe from :recipeId param and places it in res.locals
    * @param req
@@ -18,6 +37,8 @@ module.exports = {
       res.status(400).send({ errors: 'Invalid recipe id!' })
       return
     }
+
+    doc.ingredient_keywords = await module.exports.processRecipeIngredients(doc.ingredients.map(i => i.text))
 
     res.locals.recipe = doc
     next()
